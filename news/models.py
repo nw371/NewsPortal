f15f9c99dcf4578aedcff36978a2579c53d7db2a
Serge <sergey@batalov.email>
2021-08-06 15:15:36 +0100

    всё с нуля

--- news/models.py
+++ news/models.py
@@ -5,13 +5,21 @@ class Authors(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     authors_rating = models.IntegerField(default=0)
 
-    def update_rating(self):
-        #отфильтровываем все посты авотра и тянем рейтинги постов и умножаем на 3
-        #отфильтровываем все коменты авотра и тянем их рейтинги
+    def update_rating(self,authors_name):
+        #отфильтровываем все посты автора и тянем рейтинги постов, суммируем их и умножаем на 3
+        all_posts = Post.objects.filter(author = authors_name).values('post_rating')
+        all_posts = sum(all_posts)*3
+        #отфильтровываем все коменты автора и тянем их рейтинги
+        all_authors_comments = Comment.objects.filter(user = authors_name).values('comment_rating')
+        all_authors_comments = sum(all_authors_comments)
         #отфильтровываем все коменты к статьям автора и тянем их рейтинги
+        all_posts_comments = Comment.objects.filter(post = authors_name).values('comment_rating')
+        all_posts_comments = sum(all_posts_comments)
+
         #всё это суммируем
+        authors_rating = all_posts + all_authors_comments + all_posts_comments
         #сохраняем в рейтинг автора
-        pass
+        self.save()
 
 class Category(models.Model):
     category_name = models.CharField(max_length=32, default='Категория без названия')
@@ -20,11 +28,11 @@ class Post(models.Model):
     #связь «один ко многим» с моделью Author;
     author = models.ForeignKey(Authors, on_delete=models.CASCADE)
     #поле с выбором — «статья» или «новость»;
-    type_of_post = models.TextChoices('type_of_post', 'Статья Новость')
+    post_type = models.TextChoices('post_type', 'СТАТЬЯ НОВОСТЬ')
     # автоматически добавляемая дата и время создания;
-    time_of_pub = models.DateTimeField(auto_now_add=True)
+    post_time = models.DateTimeField(auto_now_add=True)
     # связь «многие ко многим» с моделью Category (с дополнительной моделью PostCategory);
-    products = models.ManyToManyField(Category, through='PostCategory')
+    post_category = models.ManyToManyField(Category, through='PostCategory')
     # заголовок статьи/новости;
     post_name =  models.CharField(max_length=255, default='Пустой заголовок')
     # текст статьи/новости;
@@ -56,28 +64,40 @@ class Comment(models.Model):
     # связь «один ко многим» с встроенной моделью User (комментарии может оставить любой пользователь, не обязательно автор);
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # текст комментария;
-    text_of_comment = models.TextField()
+    comment_text = models.TextField()
     # дата и время создания комментария;
-    time_of_comment = models.DateTimeField(auto_now_add=True)
+    comment_time = models.DateTimeField(auto_now_add=True)
     # рейтинг комментария.
-    rating_of_comment = models.IntegerField(default=0)
+    comment_rating = models.IntegerField(default=0)
 
     def like(self):
-        self.rating_of_comment += 1
+        self.comment_rating += 1
         self.save()
     def dislike(self):
-        self.rating_of_comment -= 1
+        self.comment_rating -= 1
         self.save()
 
 # SHELL COMMANDS
-# >>> from django.contrib.auth.models import User
-# >>> user1 = User.objects.create_user(username="User Name 1", password="UN1pass")
-# >>> user2 = User.objects.create_user(username="User Name 2", password="UN2pass")
-# >>> from news.models import Authors
-# >>> author1 = Authors.objects.create(author=user1)
-# >>> author2 = Authors.objects.create(author=user2)
-# >>> from news.models import Category
-# >>> cat1 = Category.objects.create(category_name='Микроэлектроника')
-# >>> cat2 = Category.objects.create(category_name='Робототехника')
-# >>> cat3 = Category.objects.create(category_name='Софт')
-# >>> cat4 = Category.objects.create(category_name='Промышленность 4.0')
+# from django.contrib.auth.models import User
+# from news.models import Authors
+# from news.models import Category
+# from news.models import Post
+# from news.models import PostCategory
+# from news.models import Comment
+
+# user1 = User.objects.create_user(username="UserName1", password="UN1pass", first_name = "User1", last_name = "Name1")
+# user2 = User.objects.create_user(username="UserName2", password="UN2pass", first_name = "User2", last_name = "Name2")
+# user3 = User.objects.create_user(username="UserName3", password="UN3pass", first_name = "User3", last_name = "Name3")
+#
+# author1 = Authors.objects.create(author=user1)
+# author2 = Authors.objects.create(author=user2)
+# author3 = Authors.objects.create(author=user3)
+
+# cat1 = Category.objects.create(category_name='Микроэлектроника')
+# cat2 = Category.objects.create(category_name='Робототехника')
+# cat3 = Category.objects.create(category_name='Софт')
+# cat4 = Category.objects.create(category_name='Промышленность 4.0')
+
+# post1 = Post.objects.create(author = author1, post_type.choices['СТАТЬЯ'], post_category = cat1, post_name = 'Новые процессоры "Эльбрус"', post_body = 'Длинный текст с описанием последнего поколения процессоров')
+# post1 = Post.objects.create(author = author1, post_category = cat1, post_name = 'Новые процессоры "Эльбрус"', post_body = 'Длинный текст с описанием последнего поколения процессоров', post_type.choices['СТАТЬЯ'], )
+# post1 = Post.objects.create(post_type.choices['СТАТЬЯ'], author = author1, post_category = cat1, post_name = 'Новые процессоры "Эльбрус"', post_body = 'Длинный текст с описанием последнего поколения процессоров')
